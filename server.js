7227b7ab33e449aacbb0fb8bf7482431e36829bd
ammar018 <ammar.ashfaq16@gmail.com>
2024-12-04 16:43:40 +0400

    commit 2

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ app.set("port", 3000);
 app.listen(app.get("port"), () => {
   console.log(`Server is running on http://localhost:${app.get("port")}`);
 });
+
+app.use((req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const timestamp = new Date();
+
+  console.log(`[${timestamp}] ${method} request to ${url}`);
+
+  res.on("finish", () => {
+    console.log(`[${timestamp}] Response status: ${res.statusCode}`);
+  });
+
+  next();
+});
