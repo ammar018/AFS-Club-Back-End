0b27c9e5f7ca458bbc7992cb794db01906b85986
ammar018 <ammar.ashfaq16@gmail.com>
2024-12-10 15:08:16 +0400

    commit 13

--- server.js
+++ server.js
@@ -34,6 +34,12 @@ app.get("/images", (req, res, next) => {
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
   next();
 });
 
